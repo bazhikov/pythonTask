6ec7ba8700e3161390d60e4e069ad691601a778d
Nurzhan Bazhikov <Nurzhan_Bazhikov@epam.com>
2019-10-04 15:45:31 +0300

    more OOP added, 1 class divided into 3 classes. Rotation logging added.

--- practising.py
+++ practising.py
@@ -1,171 +1,156 @@
 import logging
 import time
-# import os
-# from logging.handlers import TimedRotatingFileHandler
-
-class User:
-
-    def parse_param(self):
-        """Getting the employee's name and position.
-        Here is also main input questions that is addressed to Salesman
-        position. if the user types 'Manager' position, he will see the all
-        data saved from Salesman employees.
-        Also we can see the exceptions handling here, if
-        'Manager' or 'Salesman' position is not set by user during
-        the input.
-        """
-        self.name = self._input('name') # this input goes to straight to _imput method with logging
-        self.position = self._input('position') # as this
-
-        try:
-            if self.position.lower() == 'salesman': # str.lower() - allows us to avoid the issues
-                                                    # with capital letters
-
-                # here we can see the file name and 'a' means that the data will be
-                # written after the data is already. And the questions are
-                salesman_file = open('EmployeeData.txt', 'a')
-                q1 = input('Your beverage type: ')
-                q2 = input('Any additional ingredients? ')
-                q3 = input('The beverage price: ')
-
-                # below formatting is used with global attributes
-                data_name = n1 + self.name
-                salesman_file.writelines(data_name + n2 + q1 + n2 + q2 + n2 + q3 + n1)
-                salesman_file.close()
-
-            # if the user choose 'manager' position to be able to show the summary of all
-            # saved data. Here is some logging with global 'logger' parameter
-            elif self.position.lower() == 'manager':
-                logger.info('Manager is requested the summary of all salesman\'s data')
-                time.sleep(0.3) # to have the log first before it prints the data below
-                print('you\'re a great manager!')
-                f = open('EmployeeData.txt', 'r+')
-                s = f.read()
-                print('The data entered by your employees are: {}{}'.format(s, n1))
-                f.close()
-
-            # if the user is typed position different that 'salesman' or 'manager'
-            # the following lines will raise the exception and it's handled above too
-            elif self.position.lower() != 'salesman' or 'manager':
-                raise Exception('Invalid position!')
-        # handling some basic exceptions and logging it as an error
-        except (ValueError, TypeError):
-            logger.error('type once again')
-        except Exception:
-            logger.error('Please, type once again')
+import os
+from logging.handlers import TimedRotatingFileHandler
 
 
-    def _input(self, var_name):
-        """The majority of logging is here.
-        The logs are set to be displayed in console on DEBUG level and
-        to save in the file log as DEBUG level too.
-        However there is a slight difference in formatting to have the
-        difference. We can easily set only INFO logs to console and other
-        logs(as ERRORs occured by exceptions) go to the file by just changing
-        the logging level.
-        """
-        value = input('Type your {}: '.format(var_name))
+class Salesman:
+    def __init__(self, name, log):
+        self.name = name
+        self.log = log
 
-        global logger   # in order to log ERROR exception in Exception block
-        logger = logging.getLogger(value)   # will take input value to have a name column in logs
+    def process(self):
+        action = True
+        while action != 'exit':
+            action = input('What do you want? \nIf you want to add new sale info, '
+                           'please type \'sale\', unless \'exit\' \n')
+            if action == 'sale':
+                logger.info('{} wants to input the sales info'.format(self.name))
+                self.sale_beverage()
+
+            # Add additional available actions if exists with "elif action == 'action_name'"
+
+    def sale_beverage(self):
+        salesman_file = open('EmployeeData.txt', 'a')
+        q1 = input('Beverage type: ')
+        q2 = input('Any additional ingredients? ')
+        q3 = input('Beverage price: ')
+
+        # below formatting is used with global attributes
+        n1 = '\n'
+        n2 = ' | '
+        data_name = n1 + self.name
+        salesman_file.writelines(data_name + n2 + q1 + n2 + q2 + n2 + q3 + n1)
+        salesman_file.close()
+
+        # Save in file
+
+        logger.info('%s sold "%s" beverage for %s', self.name, q1, q3)
+        time.sleep(0.5)  # is to have the proper view in the console, where the logs
+        # don't appear out off time and cover the next input message
+
+    def file_formatting(self):
+
+        salesman_file = open('EmployeeData.txt', 'a')   # it is created before initializing parse_param method
+                                                        # to have the formatting Heading to the saved data
+        n1 = '\n'
+        n2 = ' | '
+        data_formatting = 'Salesman\'s name' + n2 + 'Beverage type' + n2 + 'Additional ' \
+                                                        'ingredients' + n2 + 'Price' + n1
+        salesman_file.writelines(data_formatting)
+        salesman_file.close()
+
+
+class Manager:
+    def __init__(self, name, log):
+        self.name = name
+        self.log = log
+
+    def process(self):
+        action = True
+        while action != 'exit':
+            action = input('What do you want? \nIf you want to add one more Salesman data '
+                           'please type \'salesman\'\nWould like to see all salesman data - '
+                           'type \'data\' or do \'exit\' here\n').strip()
+            if action == 'salesman':
+                client = Salesman(self.name, self.log)
+                logger.info('Manager {} wanted to add one more Employee data'.format(self.name))
+                time.sleep(0.3)
+                client.sale_beverage()
+
+            elif action == 'data':
+                logger.info('{} requested all saved employee\'s data displayed'.format(self.name))
+                time.sleep(0.3)
+                self.saved_data()
+
+    def saved_data(self):
+        # Print all salesmen
+        f = open('EmployeeData.txt', 'r+')
+        s = f.read()
+        print('The data entered by your employees are: {}\n\n'.format(s))
+        f.close()
+        # logger.info('%s requested list of salesmen', self.name)
+
+    # def get_revenue(self):
+        # Get revenue
+
+        # logger.info('%s requested revenue amount', self.name)
+
+class Logging:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+
+    def logging(self, path):
+
+        global logger  # in order to log ERROR exception in Exception block
+
+        logger = logging.getLogger(self.name)  # will take input value to have a name column in logs
         logger.setLevel(logging.DEBUG)
-        fh = logging.FileHandler('LogFile.log')    # we will transfer log info to the file
+
+        fh = TimedRotatingFileHandler(path, when='m', interval=1, backupCount=5)
+        fh.prefix = '%Y%m%d'    # can't google anything connecting with prefix, only suffix
+                                # that's why I'm afraid it's some pythonic magic, but I liked prefix
+                                # more than suffix :)
         fh.setLevel(logging.DEBUG)  # we can set the level of logging that goes to the file
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)  # we could display only INFO logs in console, however I left
-                                    # DEBUG logs here
+        # DEBUG logs here
         formatterFH = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        formatterCH = logging.Formatter('%(name)s - %(levelname)s - %(message)s')   # to differentiate
+        formatterCH = logging.Formatter('%(name)s - %(levelname)s - %(message)s')  # to differentiate
         ch.setFormatter(formatterCH)
         fh.setFormatter(formatterFH)
         logger.addHandler(ch)
         logger.addHandler(fh)
 
-        logger.info('Entered value: {} as an attribute: {}'.format(value, var_name))
-        time.sleep(0.5) # is to have the proper view in the console, where the logs
-                        # don't appear out off time and cover the next input message
-        return value
+        logger.info('Entered value: {} as an attribute: name'.format(self.name))
+        time.sleep(0.5)
+        # return value
 
-class Manager:
-    """
-    Here we have a formatting method that goes to the file first,
-    before initializing parse_param method. Salesman file is
-    created here with the heading as Salesman's name; beverage
-    type; additional ingredient and its price.
-    """
-    def file_formatting(self):
 
-        global salesman_file, n1, n2    # these attributes will be used in parse_param method as formatting too
-        salesman_file = open('EmployeeData.txt', 'a')   # it is created before initializing parse_param method
-                                                        # to have the formatting Heading to the saved data
-        n1 = '\n'
-        n2 = ' | '
-        data_formatting = n1 + 'Salesman\'s name' + n2 + 'Beverage type' + n2 + 'Additional ' \
-                                                        'ingredients' + n2 + 'Price' + n1
-        salesman_file.writelines(data_formatting)
-        salesman_file.close()
 
-def main():
-    """
-    Main method where we will ask initial question of how many employees
-    does the user want to create. It's needed to have the count to repeat
-    the method 'parse_param'
-    And here we create the objects for both classes and initialize its methods.
-    """
-    number = int(input('how many employees you will add? '))
-    saved_data = Manager()
-    saved_data.file_formatting()
+def interactive():
 
-    for i in range(number):
-        xx = User()
-        xx.parse_param()
+    name = input('What is your name? ')
+    role = input('What is your role? ')
+    client = None
+
+    log = Logging(name, role)
+    log.logging(path='.log')
+    logger.info('got the role: {}'.format(role))
+    time.sleep(0.5)
+
+
+    if role.lower() == 'salesman':
+        client = Salesman(name, log)
+        # client.logging('role')
+        client.file_formatting()
+    elif role.lower() == 'manager':
+        client = Manager(name, log)
+
+    try:
+        if client:
+            client.process()
+        elif self.position.lower() != 'salesman' or 'manager':
+            raise Exception('Invalid position!')
+
+    except (ValueError, TypeError):
+        logger.error('type once again')
+    except Exception:
+        logger.error('Invalid position! Please, type once again', interactive())
+
+
+
 
 if __name__ == '__main__':
-    main()
-
-
-
-# Below is some code to have the logging rotation, however it's not finished yet
-# and I hope to have the second chance to be reviewed once again with the following
-# additional features:
-
-    # rotation of logs: to create the log file name every day you run the script with
-        # the formatting'{}.{}.log' where the suffix is '%Y%m%d'
-
-    # connect the database: SQLite, for instance
-    # create unittests
-    # to connect listener, multiprocessing: Process, Queue, Event, current_process
-
-
-    # Rotation
-
-# class Rotation:
-#     # def __init__(self, log_directory):
-#     #     # global log_directory
-#     #     self.log_directory = log_directory
-#
-#
-#     def get_filename(self, filename):
-#         # self.filename = filename
-#         log_directory = os.path.split(filename)[0]
-#
-#         date = os.path.splitext(filename)[1][1:]
-#
-#         filename = os.path.join(log_directory, date)
-#
-#         if not os.path.exists('{}.log'.format(filename)):
-#             return '{}.log'.format(filename)
-#
-#         index = 0
-#         f = '{}.{}.log'.format(filename, index)
-#         while os.path.exists(f):
-#             index += 1
-#             f = '{}.{}.log'.format(filename, index)
-#             return f
-#
-#         rotation_logging_handler = TimedRotatingFileHandler(filename=filename, when='m', ?????????
-#                                                             interval=1, backupCount=5)
-#         rotation_logging_handler.suffix = '%Y%m%d'
-#         rotation_logging_handler.namer =
-
-
+    interactive()
